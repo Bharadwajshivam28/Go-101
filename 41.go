de4f6c5cb55b984ad61a7ece55ccedf864f9650f
shivam <shivam.kumar@matchmyflight.com>
2024-01-29 14:12:19 +0530

    go 101

---
+++ 41.go
@@ -0,0 +1,28 @@
+package main
+
+const pi = 3.1416
+
+const (
+	No = !Yes
+	Yes = true
+	MaxDegrees = 360
+	unit = "radian"
+)
+
+func main () {
+	const TwoPi, HalfPi, Unit2 = pi * 2, pi * 0.5, "degree"
+}
+
+const X float32 = 3.14
+
+const (
+	A, B int 64 = -3, 5
+	Y float32 = 2.718
+)
+
+const X = float32(3.14)
+
+const (
+	A, B = int64(-3), int64(5)
+	Y = float32(2.718)
+)
---
+++ 44.go
@@ -0,0 +1,18 @@
+package main
+
+const (
+	X float32 = 3.14
+	Y
+	Z
+
+	A, B = "Go", "language"
+	C, _
+)
+
+const (
+	X float32 = 3.14
+	Y float32 = 3.14
+	Z float32 = 3.14
+	A, B = "Go", "language"
+	C, _ = "Go", "language"
+)
